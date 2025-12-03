1ec62f2b8fe887091b15fe25394bbbab6c3af03a
stefanozuffi <stefano.zuffi06@outlook.com>
2025-12-03 14:23:35 +0100

    init: Prodotto class with methods to retrieve attributes

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,21 @@
+package org.lessons.java.shop;
+
+public class Main {
+    public static void main() {
+        Prodotto item1 = new Prodotto("Crema Idratante", 13.50f);
+        Prodotto item2 = new Prodotto("Caramelle", 7.20f);
+
+        item1.setIVA(0.4f);
+        item2.setIVA(2.78f);
+        
+        item1.setDescription("Your dermatologist first choice!");
+
+        System.out.println(item1.getPrice());
+        System.out.println(item1.getTaxedPrice());
+        System.out.println(item2.getFullID());
+
+        System.out.println(item1.description);
+
+
+    }
+}
---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,43 @@
+package org.lessons.java.shop;
+
+
+public class Prodotto {
+    public double codex;
+    public String name;
+    public String description="";
+    public float price;
+    public float iva=0.22f;
+
+    //Constructor(s)
+    public Prodotto(String name, float price) {
+        this.codex = Math.random() * 10;
+
+        this.name = name;
+        this.price = price;
+    }
+    public void setDescription(String text) {
+        this.description = text;
+    }
+    public void setIVA(float iva) {
+        if (0 < iva && iva < 1) {
+            this.iva = iva;
+        }
+    }
+
+    //Methods
+    public float getPrice() {
+        return this.price;
+    }
+    
+    public float getTaxedPrice() {
+        return (this.price * this.iva) + this.price;
+    }
+
+    public String getFullID() {
+        String codex_string = String.valueOf(this.codex);
+        return this.name.trim() + codex_string;
+    }
+
+
+
+};
